98f2ca7234451b73beffa2ef63f04c9f4ef77040
Mahak Gupta <mahakg2588@gmail.com>
2026-04-28 15:40:54 +0530

    code updated

---
+++ src/main/java/com/business/Exceptions.java
@@ -0,0 +1,14 @@
+package com.business;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class Exceptions
+{
+
+	
+
+}
